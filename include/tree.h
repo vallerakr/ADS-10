a50b7abc9c2a2ba441a153f7ad955fac394a3aa8
Kryazheva Valeria <150891739+vallerakr@users.noreply.github.com>
2024-06-09 10:59:36 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,30 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+
+class Tree {
+ private:
+    std::vector<char> elements;
+    int size = 0, permutations = 0;
+
+ public:
+    explicit Tree(const std::vector<char> vector) : elements{vector} {
+        elements = vector;
+        for (int i = 0; i < elements.size(); i++) {
+            size++;
+        }
+        if (size) {
+            permutations = 1;
+        }
+        for (int i = 2; i <= size; i++) {
+            permutations *= i;
+        }
+    }
+
+    int get_size() const { return size; }
+
+    int get_permutations() const { return permutations; }
+};
 
 #endif  // INCLUDE_TREE_H_
